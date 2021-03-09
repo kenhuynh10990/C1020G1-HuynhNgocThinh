11013de390bf9edde8fd83cda1e734a7f84f4621
kenhuynh10990 <kendo_shjnzj@yahoo.com>
2021-03-09 20:54:15 +0700

    s

--- Module4/webservice/bai-tap/blog/src/main/java/com/example/demo/controller/BlogController.java
+++ Module4/webservice/bai-tap/blog/src/main/java/com/example/demo/controller/BlogController.java
@@ -18,114 +18,114 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-//@RestController
-@Controller
+@RestController
+//@Controller
 public class BlogController {
     @Autowired
     private BlogService blogService;
     @Autowired
     private CategoriesService categoriesService;
 
-    @GetMapping("/create")
-    public ModelAndView showCreateForm(Pageable pageable) {
-        ModelAndView modelAndView = new ModelAndView("/create");
-        modelAndView.addObject("categories", categoriesService.findAll(pageable));
-        modelAndView.addObject("blog", new Blog());
-        return modelAndView;
-    }
 
-    //
-    @PostMapping("/create")
-    public ModelAndView save(Blog blog) {
-        blog.setDate(new Date());
-        blogService.save(blog);
-        ModelAndView modelAndView = new ModelAndView("redirect:/");
-
-        modelAndView.addObject("blog", new Blog());
-
-        return modelAndView;
-    }
-
-    //    @GetMapping("/")
-//    public ResponseEntity<List<Blog>> listAllBlog(){
-//        List<Blog> blogs = blogService.findAll();
-//        if (blogs.isEmpty()){
-//            return new ResponseEntity<List<Blog>>(HttpStatus.NO_CONTENT);
-//        }else {
-//            return new ResponseEntity<List<Blog>>(blogs,HttpStatus.OK);
-//        }
-//    }
-//
-//    @GetMapping("/view/{id}")
-//    public ResponseEntity<Blog> viewBlog(@PathVariable int id){
-//        Blog blog =blogService.findById(id);
-//        if (blog==null){
-//            return new ResponseEntity<Blog>(HttpStatus.NO_CONTENT);
-//        }else {
-//            return new ResponseEntity<Blog>(blog,HttpStatus.OK);
-//        }
-//    }
     @GetMapping("/")
-    public String list(Model model, @PageableDefault(size = 2, sort = "date") Pageable pageable, @RequestParam Optional<String> textSearch) {
-        if (textSearch.isPresent()) {
-            model.addAttribute("blog", blogService.findAllByTextContaining(textSearch.get(), pageable));
-            return "/list";
-        } else {
-            model.addAttribute("blog", blogService.findAll(pageable));
-//            model.addAttribute("category", categoriesService.findAll(pageable));
-            return "/list";
+    public ResponseEntity<List<Blog>> listAllBlog(){
+        List<Blog> blogs = blogService.findAll();
+        if (blogs.isEmpty()){
+            return new ResponseEntity<List<Blog>>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<List<Blog>>(blogs,HttpStatus.OK);
         }
     }
 
-//    @RequestMapping(value="/setSearch/search", method=RequestMethod.POST)
-//    public @ResponseBody String search (@RequestParam String textSearch, ModelMap model,Pageable pageable)
-//    {
-//        Page<Blog> blogs =  blogService.findAllByTextContaining(textSearch,pageable);
-//        model.put("searchResult", questionAnswerSetByQuestion);
+    @GetMapping("/view/{id}")
+    public ResponseEntity<Blog> viewBlog(@PathVariable int id){
+        Blog blog =blogService.findById(id);
+        if (blog==null){
+            return new ResponseEntity<Blog>(HttpStatus.NO_CONTENT);
+        }else {
+            return new ResponseEntity<Blog>(blog,HttpStatus.OK);
+        }
+    }
+
+
+
+//    @GetMapping("/create")
+//    public ModelAndView showCreateForm(Pageable pageable) {
+//        ModelAndView modelAndView = new ModelAndView("/create");
+//        modelAndView.addObject("categories", categoriesService.findAll(pageable));
+//        modelAndView.addObject("blog", new Blog());
+//        return modelAndView;
+//    }
 //
-//        return "setSearch";
+//    //
+//    @PostMapping("/create")
+//    public ModelAndView save(Blog blog) {
+//        blog.setDate(new Date());
+//        blogService.save(blog);
+//        ModelAndView modelAndView = new ModelAndView("redirect:/");
+//
+//        modelAndView.addObject("blog", new Blog());
+//
+//        return modelAndView;
 //    }
 
-    @GetMapping("/edit/{id}")
-    public String showEditForm(@PathVariable int id, Model model, Pageable pageable) {
-        Blog blog = blogService.findById(id);
-        if (blog != null) {
-            model.addAttribute("categories", categoriesService.findAll(pageable));
-            model.addAttribute("blog", blog);
-            return "/edit";
-        } else {
-            return "/error";
-        }
-    }
-
-    @PostMapping("/edit")
-    public String update(Blog blog, Model model) {
-        blogService.save(blog);
-        model.addAttribute("blog", blog);
-        model.addAttribute("message", "Blog updated");
-        return "redirect:/";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String showDeleteForm(@PathVariable int id, Model model) {
-        Blog blog = blogService.findById(id);
-        if (blog != null) {
-            model.addAttribute("blog", blog);
-            return "/delete";
-        } else {
-            return "/delete";
-        }
-    }
-
-    @PostMapping("/delete")
-    public String delete(Blog blog) {
-        blogService.remove(blog.getId());
-        return "redirect:/";
-    }
 
 
-    public String viewForm(@PathVariable int id, Model model) {
-        model.addAttribute("blog", blogService.findById(id));
-        return "/viewBlog";
-    }
+
+//    @GetMapping("/")
+//    public String list(Model model, @PageableDefault(size = 2, sort = "date") Pageable pageable, @RequestParam Optional<String> textSearch) {
+//        if (textSearch.isPresent()) {
+//            model.addAttribute("blog", blogService.findAllByTextContaining(textSearch.get(), pageable));
+//            return "/list";
+//        } else {
+//            model.addAttribute("blog", blogService.findAll(pageable));
+////            model.addAttribute("category", categoriesService.findAll(pageable));
+//            return "/list";
+//        }
+//    }
+
+
+
+//    @GetMapping("/edit/{id}")
+//    public String showEditForm(@PathVariable int id, Model model, Pageable pageable) {
+//        Blog blog = blogService.findById(id);
+//        if (blog != null) {
+//            model.addAttribute("categories", categoriesService.findAll(pageable));
+//            model.addAttribute("blog", blog);
+//            return "/edit";
+//        } else {
+//            return "/error";
+//        }
+//    }
+//
+//    @PostMapping("/edit")
+//    public String update(Blog blog, Model model) {
+//        blogService.save(blog);
+//        model.addAttribute("blog", blog);
+//        model.addAttribute("message", "Blog updated");
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/delete/{id}")
+//    public String showDeleteForm(@PathVariable int id, Model model) {
+//        Blog blog = blogService.findById(id);
+//        if (blog != null) {
+//            model.addAttribute("blog", blog);
+//            return "/delete";
+//        } else {
+//            return "/delete";
+//        }
+//    }
+//
+//    @PostMapping("/delete")
+//    public String delete(Blog blog) {
+//        blogService.remove(blog.getId());
+//        return "redirect:/";
+//    }
+//
+//
+//    public String viewForm(@PathVariable int id, Model model) {
+//        model.addAttribute("blog", blogService.findById(id));
+//        return "/viewBlog";
+//    }
 }
