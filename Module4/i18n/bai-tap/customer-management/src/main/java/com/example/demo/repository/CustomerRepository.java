95c1cc4e4c0caa12f058b23ae6fb0299cd863e29
kenhuynh10990 <kendo_shjnzj@yahoo.com>
2021-03-11 10:39:01 +0700

    s

---
+++ Module4/i18n/bai-tap/customer-management/src/main/java/com/example/demo/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Customer;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends PagingAndSortingRepository<Customer,Integer> {
+}
